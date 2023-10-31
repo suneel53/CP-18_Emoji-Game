c07af4932a84c9f71c78aa7a1cf140043b6fd7cc
Suneel <80934950+suneel53@users.noreply.github.com>
2023-11-01 00:10:23 +0530

    Update index.js

--- src/components/EmojiCard/index.js
+++ src/components/EmojiCard/index.js
@@ -10,8 +10,10 @@ class EmojiCard extends Component {
       addSelectedEmoji(id)
     }
     return (
-      <li className="emojilist-cont" onClick={addId}>
-        <img src={emojiUrl} alt={emojiName} />
+      <li className="emojilist-cont">
+        <button type="button" onClick={addId} className="emojicard-but">
+          <img src={emojiUrl} alt={emojiName} />
+        </button>
       </li>
     )
   }
